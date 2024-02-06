1ce535cff8610251c5b84070047ecb0074a5fa04
Juho Hämäläinen <jusa@hilvi.org>
2024-02-06 09:28:35 +0200

    sdk.py: Format task list based on terminal size.

--- sdk.py
+++ sdk.py
@@ -96,12 +96,20 @@ def print_tasks(monitor=False):
         # not best but shortest solution for now
         os.system("clear")
     if len(tasks) > 0:
-        print("\x1b[30;107m{0:6s}\x1b[39;49m \x1b[30;107m{1:12s}\x1b[39;49m \x1b[30;107m{2:24s}\x1b[39;49m".format("[id/s]", "[path]", "[cmdline]"))
-        for idno, state, full_path, cmd, ret, duration in tasks:
-            run_path = ''.join(full_path.split("/")[-1:])
-            if len(run_path) > 12:
-                run_path = ".." + run_path[-10:]
-            line = "{0:3d} {1:<2s} {2:12s} {3:s}".format(idno, state_short_str(state), run_path, cmd)
+        col, _ = os.get_terminal_size()
+        path_width = int(col / 3)
+        cmd_width = col - 8 - path_width
+        print("\x1b[30;107m{0:6s}\x1b[39;49m ".format("[id/s]"), end='')
+        print("\x1b[30;107m{0}{1}\x1b[39;49m ".format("[path]", " " * (path_width - 6)), end='')
+        print("\x1b[30;107m{0}{1}\x1b[39;49m ".format("[cmdline]", " " * (cmd_width - 9)))
+        for idno, state, run_path, cmd, ret, duration in tasks:
+            if len(run_path) > path_width:
+                run_path = ".." + run_path[-(path_width - 2):]
+            else:
+                run_path = run_path + (path_width - len(run_path)) * " "
+            if len(cmd) > cmd_width:
+                cmd = cmd[:(cmd_width - 2)] + ".."
+            line = "{0:3d} {1:<2s} {2} {3}".format(idno, state_short_str(state), run_path, cmd)
             print(LOG_STR[state].format(line))
     elif monitor:
         print("No tasks.")
